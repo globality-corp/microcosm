ddfd5212646bf1378f8e8d858f5ccc7fa3e1dd65
cdm-ium <constantine.dm@gmail.com>
2016-08-25 21:01:16 -0700

    adding extra test

--- microcosm/tests/test_opaque.py
+++ microcosm/tests/test_opaque.py
@@ -53,7 +53,6 @@ def test_decorator():
 
 def test_nesting():
     opaque = Opaque()
-
     opaque['some'] = 'cool stuff'
 
     assert_that(opaque.as_dict(), is_(equal_to(dict(some='cool stuff'))))
@@ -62,4 +61,44 @@ def test_nesting():
         with opaque.bind(special_context_function, 'some more', 'stuff'):
             assert_that(opaque.as_dict(), is_(equal_to(special_context_function('some more', 'stuff'))))
         assert_that(opaque.as_dict(), is_(equal_to(special_context_function('some', 'stuff'))))
+    assert_that(opaque.as_dict(), is_(equal_to(dict(some='cool stuff'))))
+
+
+def test_collaboration():
+    """
+    This test is intended to show how to use Opaque with other microcosm components. Here `Apple` is
+    a component that benefits from context awareness, but it does not know where it will be
+    called from.
+
+    The Juicer will call the Apple while in its special context. Because the Apple has a
+    reference to opaque and the Juicer's class function uses opaque.bind to apply context to it
+    the Apple is able to refer to the context it is being called from.
+
+    """
+    opaque = Opaque()
+    opaque['some'] = 'cool stuff'
+
+    class Apple(object):
+        def __init__(self, context):
+            self.opaque = context
+
+        def show_opaque(self):
+            return self.opaque.as_dict()
+
+    class Juicer(object):
+        def __init__(self, apple):
+            self.apple = apple
+
+        def do_work(self):
+            return apple.show_opaque()
+
+    apple = Apple(opaque)
+    juicer = Juicer(apple)
+
+    decorated_work = opaque.bind(
+        special_context_function, 'some', 'stuff'
+    )(juicer.do_work)
+
+    assert_that(opaque.as_dict(), is_(equal_to(dict(some='cool stuff'))))
+    assert_that(decorated_work(), is_(equal_to(special_context_function('some', 'stuff'))))
     assert_that(opaque.as_dict(), is_(equal_to(dict(some='cool stuff'))))
