780f667a77328db31d11058ce970971b899b6918
Salman Mashayekh <salman.mashayekh@globality.com>
2019-08-05 11:31:05 -0700

    expose the utility function through the microcosm.api

--- microcosm/api.py
+++ microcosm/api.py
@@ -1,12 +1,13 @@
 from microcosm.config.validation import required, typed
 from microcosm.decorators import binding, defaults
 from microcosm.loaders import load_each, load_from_dict, load_from_environ
-from microcosm.object_graph import create_object_graph
+from microcosm.object_graph import create_object_graph, get_component_name
 
 
 __all__ = [
     "binding",
     "create_object_graph",
+    "get_component_name",
     "defaults",
     "load_each",
     "load_from_dict",
